1bfaee5984fe4661fa4ed8a4c95165c7dff21287
JuliaNowakowska17 <nowakowskajulia17@gmail.com>
2026-03-24 16:15:58 +0100

    Add files via upload

---
+++ Requests/src/main/java/lab/requests/data/EntityManagerProducer.java
@@ -0,0 +1,26 @@
+package lab.requests.data;
+
+import jakarta.annotation.Priority;
+import jakarta.enterprise.context.Dependent;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Dependent
+@Alternative
+@Priority(1)
+
+public class EntityManagerProducer {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Produces
+    @RequestScoped
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+}
